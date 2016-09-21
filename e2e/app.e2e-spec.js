5dc04b14b603860bce6e93fd40352bb7638ee1ae
gazbert <gareth.jon.lynch@gmail.com>
2016-09-21 23:32:19 +0100

    Plain backgrounds are boring...

--- e2e/app.e2e-spec.js
+++ e2e/app.e2e-spec.js
@@ -1,14 +1,10 @@
 describe('BX-bot Admin E2E Tests', function () {
-
     var expectedMsg = 'BX-bot Admin';
-
     beforeEach(function () {
         browser.get('');
     });
-
     it('should display: ' + expectedMsg, function () {
         expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
     });
-
 });
 //# sourceMappingURL=app.e2e-spec.js.map
