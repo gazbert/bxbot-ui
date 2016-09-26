e2201317e309cedf2ee9280997696e146c4f2468
gazbert <gareth.jon.lynch@gmail.com>
2016-09-26 22:33:29 +0100

    A small but great improvement

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,13 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'bxbot-ui',
     template: `
+    <div class="container">
     <h1>{{title}}</h1>
     <nav>
         <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>       
     </nav>
     <router-outlet></router-outlet>
+    </div>
   `,
     styleUrls: ['app/app.component.css']
 })
