7ebcde03d2ed7db075bf6042bbb414fb0b58dfc5
gazbert <gareth.jon.lynch@gmail.com>
2016-12-22 20:25:07 +0000

    Removed duplicate test

--- e2e/dashboard.e2e-spec.ts
+++ e2e/dashboard.e2e-spec.ts
@@ -67,32 +67,3 @@ describe('Dashboard Tests', function () {
     });
 });
 
-/**
- * Exchange Details screen tests.
- *
- * TODO - Test for Dashboard click
- * TODO - Tests for nav to Markets, Strats, Email Alerts
- */
-describe('Exchange Details Tests', function () {
-
-    beforeEach(function () {
-        browser.get('');
-    });
-
-    it('Should render GDAX Exchange specific details', function () {
-
-        let dashboardItems = element.all(by.css('bx-dashboard-item'));
-        dashboardItems.get(1).click();
-        expect(element(by.css('h2')).getText()).toEqual('GDAX Exchange Details');
-
-        let tabLinks = element.all(by.css('li'));
-        expect(tabLinks.count()).toBe(4);
-        expect(tabLinks.first().getText()).toEqual('Exchange Adapter');
-        expect(tabLinks.get(1).getText()).toEqual('Markets');
-        expect(tabLinks.get(2).getText()).toEqual('Trading Strategies');
-        expect(tabLinks.last().getText()).toEqual('Email Alerts');
-
-        let tabItems = element.all(by.css('tab'));
-        expect(tabItems.count()).toBe(4);
-    });
-});
