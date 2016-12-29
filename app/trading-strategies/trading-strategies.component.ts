42c7178aac43f15211c27548e93dfb0bd68814ac
gazbert <gareth.jon.lynch@gmail.com>
2016-12-29 15:09:25 +0000

    Fixed lint issues

--- app/trading-strategies/trading-strategies.component.ts
+++ app/trading-strategies/trading-strategies.component.ts
@@ -112,7 +112,7 @@ export class TradingStrategiesComponent implements OnInit {
     // Algo by @Broofa - http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript/2117523#2117523
     createUuid() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            let r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
     }
