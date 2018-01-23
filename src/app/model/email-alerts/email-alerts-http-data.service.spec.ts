b328e3c27f5b0e58f12478e797bbf83d2de35465
gazzbert <gareth.jon.lynch@gmail.com>
2018-01-23 17:33:27 +0000

    Refactored email alerts model tests

--- src/app/model/email-alerts/email-alerts-http-data.service.spec.ts
+++ src/app/model/email-alerts/email-alerts-http-data.service.spec.ts
@@ -1,128 +1,128 @@
-// import {MockBackend, MockConnection} from '@angular/http/testing';
-// import {Http, HttpModule, Response, ResponseOptions, XHRBackend} from '@angular/http';
-// import {async, inject, TestBed} from '@angular/core/testing';
-// import {EmailAlertsHttpDataService as EmailAlertsDataService} from './email-alerts-http-data.service';
-// import {EmailAlertsConfig, SmtpConfig} from './email-alerts.model';
-//
-// /**
-//  * Tests the Email Alerts HTTP Data service using a mocked HTTP backend.
-//  *
-//  * TODO - test non 200 OK responses etc from bxbot-ui-server - UI should handle scenario gracefully!
-//  *
-//  * @author gazbert
-//  */
-// describe('EmailAlertsHttpDataService tests using TestBed + Mock HTTP backend', () => {
-//
-//     beforeEach(async(() => {
-//         TestBed.configureTestingModule({
-//             imports: [HttpModule],
-//             providers: [
-//                 EmailAlertsDataService,
-//                 {provide: XHRBackend, useClass: MockBackend}
-//             ]
-//         })
-//             .compileComponents().then(() => {/*done*/
-//         });
-//     }));
-//
-//     it('should instantiate implementation of EmailAlertsDataService when injected',
-//         inject([EmailAlertsDataService], (service: EmailAlertsDataService) => {
-//             expect(service instanceof EmailAlertsDataService).toBe(true);
-//         }));
-//
-//     it('should instantiate service with "new"', inject([Http], (http: Http) => {
-//         expect(http).not.toBeNull('http should be provided');
-//         const service = new EmailAlertsDataService(http);
-//         expect(service instanceof EmailAlertsDataService).toBe(true,
-//             'new service should be instance of EmailAlertsDataService');
-//     }));
-//
-//     it('should provide the MockBackend as XHRBackend',
-//         inject([XHRBackend], (backend: MockBackend) => {
-//             expect(backend).not.toBeNull('MockBackend backend should be provided');
-//         }));
-//
-//     describe('when getEmailAlertsConfigByBotId() operation called with \'1\'', () => {
-//
-//         let backend: MockBackend;
-//         let service: EmailAlertsDataService;
-//         let fakeEmailAlertsConfig: EmailAlertsConfig;
-//         let response: Response;
-//
-//         beforeEach(inject([Http, XHRBackend], (http: Http, be: MockBackend) => {
-//             backend = be;
-//             service = new EmailAlertsDataService(http);
-//             fakeEmailAlertsConfig = makeEmailAlertsData();
-//             const options = new ResponseOptions({status: 200, body: {data: fakeEmailAlertsConfig}});
-//             response = new Response(options);
-//         }));
-//
-//         it('should expect GDAX Email Alerts config', async(inject([], () => {
-//             backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
-//             service.getEmailAlertsConfigByBotId('gdax-1')
-//                 .then(emailAlertsConfig => {
-//                     expect(emailAlertsConfig.id).toBe('gdax-1');
-//                     expect(emailAlertsConfig.enabled).toBe(true);
-//                     expect(emailAlertsConfig.smtpConfig.smtpHost).toBe('smtp.gmail.com');
-//                     expect(emailAlertsConfig.smtpConfig.smtpPort).toBe(587);
-//                     expect(emailAlertsConfig.smtpConfig.accountUsername).toBe('bobfett');
-//                     expect(emailAlertsConfig.smtpConfig.accountPassword).toBe('iLoveHoth');
-//                     expect(emailAlertsConfig.smtpConfig.fromAddress).toBe('boba.fett@hoth.space');
-//                     expect(emailAlertsConfig.smtpConfig.toAddress).toBe('jabba@tatooine.space');
-//                 });
-//         })));
-//
-//         it('should handle returning no matching Email Alerts config', async(inject([], () => {
-//             const resp = new Response(new ResponseOptions({status: 200, body: {data: []}}));
-//             backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
-//             service.getEmailAlertsConfigByBotId('unknown')
-//                 .then(emailAlertsConfig => expect(emailAlertsConfig).not.toBeDefined('should have no Email Alerts config'));
-//         })));
-//     });
-//
-//     describe('when updateEmailAlertsConfig() operation called', () => {
-//
-//         let backend: MockBackend;
-//         let service: EmailAlertsDataService;
-//         let response: Response;
-//         let updatedEmailAlertsConfig: EmailAlertsConfig;
-//
-//         beforeEach(inject([Http, XHRBackend], (http: Http, be: MockBackend) => {
-//
-//             updatedEmailAlertsConfig = new EmailAlertsConfig('gdax-1', true,
-//                 new SmtpConfig('new.smtp.gmail.com', 589, 'new_bobfett', 'new_iLoveHoth',
-//                     'new_jabba@tatooine.space', 'new_boba.fett@hoth.space'));
-//
-//             backend = be;
-//             service = new EmailAlertsDataService(http);
-//             const options = new ResponseOptions({status: 200, body: {data: updatedEmailAlertsConfig}});
-//             response = new Response(options);
-//         }));
-//
-//         it('should return updated GDAX Email Alerts config on success', async(inject([], () => {
-//             backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
-//             service.updateEmailAlertsConfig('gdax', updatedEmailAlertsConfig)
-//                 .then(emailAlertsConfig => {
-//                     expect(emailAlertsConfig).toBe(updatedEmailAlertsConfig);
-//
-//                     // paranoia!
-//                     expect(emailAlertsConfig.id).toBe('gdax-1');
-//                     expect(emailAlertsConfig.smtpConfig.smtpHost).toBe('new.smtp.gmail.com');
-//                 });
-//         })));
-//
-//         it('should NOT return Email Alerts config for 401 response', async(inject([], () => {
-//             const resp = new Response(new ResponseOptions({status: 401, body: {data: ['Bad request - unknown id']}}));
-//             backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
-//             service.updateEmailAlertsConfig('gdax', updatedEmailAlertsConfig)
-//                 .then(emailAlertsConfig => expect(emailAlertsConfig.id).not.toBeDefined('should not have Email Alerts config'));
-//         })));
-//     });
-// });
-//
-// function makeEmailAlertsData() {
-//     return new EmailAlertsConfig('gdax-1', true,
-//         new SmtpConfig('smtp.gmail.com', 587, 'bobfett', 'iLoveHoth', 'jabba@tatooine.space', 'boba.fett@hoth.space'));
-// }
-//
-//
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpClient} from '@angular/common/http';
+import {async, inject, TestBed} from '@angular/core/testing';
+import {EmailAlertsHttpDataService as EmailAlertsDataService} from './email-alerts-http-data.service';
+import {EmailAlertsConfig, SmtpConfig} from './email-alerts.model';
+
+/**
+ * Tests the Email Alerts HTTP Data service using a mocked HTTP backend.
+ *
+ * @author gazbert
+ */
+describe('EmailAlertsHttpDataService tests using HttpClientTestingModule', () => {
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [EmailAlertsDataService]
+        });
+    }));
+
+    afterEach(inject([HttpTestingController], (backend: HttpTestingController) => {
+        backend.verify();
+    }));
+
+    describe('when getEmailAlertsConfigByBotId() operation called with \'gdax-1\'', () => {
+
+        let backend: HttpTestingController;
+        let service: EmailAlertsDataService;
+        let emailAlertsConfig: EmailAlertsConfig;
+
+        beforeEach(inject([HttpClient, HttpTestingController], (http: HttpClient, testController: HttpTestingController) => {
+            backend = testController;
+            service = new EmailAlertsDataService(http);
+            emailAlertsConfig = makeEmailAlertsData();
+        }));
+
+        it('should expect GDAX Email Alerts config', async(inject([], () => {
+
+            service.getEmailAlertsConfigByBotId('gdax-1')
+                .then(response => {
+                    expect(response).toBe(emailAlertsConfig);
+
+                    // paranoia!
+                    expect(response.smtpConfig.accountUsername).toEqual('bobfett');
+                });
+
+            backend.expectOne({
+                url: 'app/email_alerts?botId=gdax-1',
+                method: 'GET'
+            }).flush(emailAlertsConfig, {status: 200, statusText: 'Ok'});
+
+        })));
+
+        it('should handle returning no matching Email Alerts config', async(inject([], () => {
+
+            service.getEmailAlertsConfigByBotId('gdax-unknown')
+                .then(engine => {
+                    expect(engine).toBeUndefined();
+                });
+
+            backend.expectOne({
+                url: 'app/email_alerts?botId=gdax-unknown',
+                method: 'GET'
+            }).flush([], {status: 200, statusText: 'Ok'});
+
+        })));
+    });
+
+    describe('when updateEmailAlertsConfig() operation called', () => {
+
+        let backend: HttpTestingController;
+        let service: EmailAlertsDataService;
+        let updatedEmailAlertsConfig: EmailAlertsConfig;
+
+        beforeEach(inject([HttpClient, HttpTestingController], (http: HttpClient, testController: HttpTestingController) => {
+
+            updatedEmailAlertsConfig = new EmailAlertsConfig('gdax-1', true,
+                new SmtpConfig('new.smtp.gmail.com', 589, 'new_bobfett', 'new_iLoveHoth',
+                    'new_jabba@tatooine.space', 'new_boba.fett@hoth.space'));
+
+            backend = testController;
+            service = new EmailAlertsDataService(http);
+        }));
+
+        it('should return updated GDAX Email Alerts config on success', async(inject([], () => {
+
+            service.updateEmailAlertsConfig('gdax-1', updatedEmailAlertsConfig)
+                .then(response => {
+                    expect(response).toBe(updatedEmailAlertsConfig);
+
+                    // paranoia!
+                    expect(response.smtpConfig.accountUsername).toBe('new_bobfett');
+                });
+
+            backend.expectOne({
+                url: 'app/email_alerts/gdax-1',
+                method: 'PUT'
+            }).flush(updatedEmailAlertsConfig, {status: 200, statusText: 'Ok'});
+
+        })));
+
+        it('should NOT return Email Alerts config for 401 response', async(inject([], () => {
+
+            const unknownEmailAlertsConfig = new EmailAlertsConfig('gdax-unknown', true,
+                new SmtpConfig('new.smtp.gmail.com', 589, 'new_bobfett', 'new_iLoveHoth',
+                    'new_jabba@tatooine.space', 'new_boba.fett@hoth.space'));
+
+            service.updateEmailAlertsConfig('gdax-unknown', unknownEmailAlertsConfig)
+                .then(response => {
+                    expect(response.smtpConfig).toBeUndefined();
+                });
+
+            backend.expectOne({
+                url: 'app/email_alerts/gdax-unknown',
+                method: 'PUT'
+            }).flush({status: 404, statusText: 'Not Found'});
+
+        })));
+    });
+});
+
+function makeEmailAlertsData() {
+    return new EmailAlertsConfig('gdax-1', true,
+        new SmtpConfig('smtp.gmail.com', 587, 'bobfett', 'iLoveHoth',
+            'jabba@tatooine.space', 'boba.fett@hoth.space'));
+}
+
+
