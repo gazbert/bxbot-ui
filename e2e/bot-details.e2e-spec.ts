c1a64730b0003d47f1ddfeb25be6c4db21323856
gazbert <gareth.jon.lynch@gmail.com>
2018-01-27 16:47:47 +0000

    Disable e2e test for now

--- e2e/bot-details.e2e-spec.ts
+++ e2e/bot-details.e2e-spec.ts
@@ -12,7 +12,7 @@ import {browser, element, by, protractor} from 'protractor';
  *
  * @author gazbert
  */
-describe('Bot Details Tests', function () {
+xdescribe('Bot Details Tests', function () {
 
     const WAIT_TIMEOUT = 300000;
 
